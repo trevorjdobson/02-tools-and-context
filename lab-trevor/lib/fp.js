55b6c8ee2d1fe8b96e1ec7fbd54888249d9c6e0d
trevorjdobson <trevorjdobson@gmail.com>
2018-02-08 20:04:12 -0800

    functions written and testing properly

---
+++ lab-trevor/lib/fp.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const Functional = {};
+
+Functional.myForEach = (arr, cb) => {
+    for(let i = 0; i < arr.length; i++) {
+        let el = arr[i];
+        cb(el)
+    }
+};
+
+Functional.myMap = (arr, cb) => {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        let el = arr[i];
+        let result = cb(el);
+        newArr.push(result);
+    }
+    return newArr;
+};
+
+Functional.myFilter = (arr, cb) => {
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        let el = arr[i];
+        if (cb(el) === true){
+        newArr.push(el);
+        }
+    }
+    return newArr;
+};
+
+Functional.myReduce = (arr, cb, start=0) => {
+    let newArr = start;
+    for(let i = 0; i < arr.length; i++){
+        newArr = cb(newArr, arr[i]);
+    }
+    return newArr;
+};
+
+module.exports = {};
+module.exports.Functional = Functional;
---
+++ lab-trevor/test/fp.test.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const functional = require('../lib/fp.js').Functional
+
+// describe("sum tests", () => {
+//     it("it should return 0 for an empty arrary", () => {
+        
+//         expect(77).toBe(77);
+//     })
+// })
+
+describe("testing functions", () => {
+    describe("testing the for each function", () => {
+        const arr = [1, 2, 3,];
+        it("it should double each element in the arr", () => {
+            let expected = [2, 4, 6];
+            const double = (n) => {return n * 2};
+            let result = functional.myMap(arr, double);
+            expect(result).toEqual(expected);
+        })
+    })
+    describe("testing the map function", () => {
+        const arr = [1, 2, 3]
+        it("should double all elements in arr return [2, 4, 6]", () => {
+            let expected = [2, 4, 6];
+            const double = (n) => {return n * 2};
+            let result = functional.myMap(arr, double);
+            expect(result).toEqual(expected);
+        })
+    })
+    describe("testing the filter function", () => {
+        const arr = [3, 8, 12, 13]
+        it("should return arr with only even numbers", () => {
+            let expected = [8, 12];
+            const even = (n) => {return n % 2 ===0};
+            let result = functional.myFilter(arr, even);
+            expect(result).toEqual(expected);
+        })
+    })
+    describe("testing the reduce function", () => {
+        const arr = [2, 5, 8, 15]
+        it("should return array that summed the values of original arr", () => {
+            let expected = [30];
+            let result = functional.myReduce(arr, (x, y) => {x + y});
+        })
+    })
+})
